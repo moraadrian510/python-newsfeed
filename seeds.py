62e978555aa0475b93fbfc12ea08c0f543215a5e
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-06-09 16:16:37 -0700

    added ccomments to seeds.py

--- seeds.py
+++ seeds.py
@@ -1,5 +1,6 @@
-from app.models import User, Post
+from app.models import User, Post, Comment
 from app.db import Session, Base, engine
+
 
 # drop and rebuild tables
 Base.metadata.drop_all(engine)
@@ -18,8 +19,6 @@ db.add_all([
 
 db.commit()
 
-db.close()
-
 # insert posts
 db.add_all([
   Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
@@ -30,3 +29,16 @@ db.add_all([
 ])
 
 db.commit()
+
+# insert comments
+db.add_all([
+  Comment(comment_text='Nunc rhoncus dui vel sem.', user_id=1, post_id=2),
+  Comment(comment_text='Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', user_id=1, post_id=3),
+  Comment(comment_text='Aliquam erat volutpat. In congue.', user_id=2, post_id=1),
+  Comment(comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', user_id=2, post_id=3),
+  Comment(comment_text='In hac habitasse platea dictumst.', user_id=3, post_id=3)
+])
+
+db.commit()
+
+db.close()
