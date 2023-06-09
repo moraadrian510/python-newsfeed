31257e833789681226d3cf827731113b85bc03a9
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-06-08 22:07:40 -0700

    added comments relationship

--- app/models/Post.py
+++ app/models/Post.py
@@ -4,12 +4,14 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
 class Post(Base):
-  __tablename__ = 'posts'
-  id = Column(Integer, primary_key=True)
-  title = Column(String(100), nullable=False)
-  post_url = Column(String(100), nullable=False)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  created_at = Column(DateTime, default=datetime.now)
-  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    post_url = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
-  user = relationship('User')
+    user = relationship('User')
+
+    comments = relationship('Comment', cascade='all,delete')
